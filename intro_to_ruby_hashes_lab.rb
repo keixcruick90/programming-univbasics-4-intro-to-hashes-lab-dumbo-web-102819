9b822b5f3aedaa815eb59a1a9ae8cf6666267d95
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-11 19:21:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,35 @@
 def new_hash
   # return an empty hash
+  Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  {
+    "green" => "not a creative color"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {
+    :name => "Grace Hopper"
+  }
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+    {
+      :id => 2
+    }
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  woman = { :name => "Grace Hopper",
+            :occupation => "explorer"}
+      woman[:name]
+      woman[:occupation]
 end
 
 def read_from_hash(hash, key)
